06a484b53e4f8657c2cdf0afa80ac2acac91c7df
nicola.cristian <nicola.cristian@TAVE-PC19.OSDBSESTO.local>
2021-04-15 13:19:19 +0200

    add file.ser

---
+++ src/serializzazione/Studente.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serializzazione;
+
+/**
+ *
+ * @author nicola.cristian
+ */
+public class Studente {
+    
+    private String nome,cognome;
+    private int num_registro;
+
+    public Studente(String nome, String cognome, int num_registro) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.num_registro = num_registro;
+    }
+
+
+
+    
+}
